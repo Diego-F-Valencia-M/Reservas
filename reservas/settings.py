5f90f7f0b3256af109a95cf7f425583221145ae0
Diego Fernando Valencia Montalvo <32377025+Diego-F-Valencia-M@users.noreply.github.com>
2025-07-28 15:13:29 -0500

    Update settings.py

--- reservas/settings.py
+++ reservas/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +78,7 @@ WSGI_APPLICATION = 'reservas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.config(default=os.environ.get('DATABASE_URL')),
     }
 }
 
