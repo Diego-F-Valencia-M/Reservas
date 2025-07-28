e43f00acfa3b3af75409a05dd6e09d426427c300
Diego-F-Valencia-M <dfvm78@live.com>
2025-07-28 17:48:13 -0500

    se corrige la redirecion para el logout 2

--- reservas/settings.py
+++ reservas/settings.py
@@ -131,4 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 LOGIN_REDIRECT_URL = '/panel/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/accounts/logout/'
